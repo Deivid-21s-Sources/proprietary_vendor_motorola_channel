ef1450af0d01ef8ba3ca0c1b87cd8d65bf677280
Nolen Johnson <johnsonnolen@gmail.com>
2024-03-30 18:33:34 -0400

    channel: Drop MotorolaSettingsProvider
    * Crashes hard on QPR2, not even needed for MotCamera.
    
    Change-Id: I677a432b070118c98cb5a930373abab714405a2a

--- channel-vendor.mk
+++ channel-vendor.mk
@@ -246,6 +246,3 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/channel/proprietary/vendor/lib/soundfx/libspeakerbundle.so:$(TARGET_COPY_OUT_VENDOR)/lib/soundfx/libspeakerbundle.so \
     vendor/motorola/channel/proprietary/vendor/lib/vndk/libtinyalsa.so:$(TARGET_COPY_OUT_VENDOR)/lib/vndk/libtinyalsa.so \
     vendor/motorola/channel/proprietary/vendor/lib64/sensors.ssc.so:$(TARGET_COPY_OUT_VENDOR)/lib64/sensors.ssc.so
-
-PRODUCT_PACKAGES += \
-    MotorolaSettingsProvider
